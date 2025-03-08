52311f011bd92fd601181ad745167766d536011e
Alexander Ortiz <alexanderortiz.201@gmail.com>
2025-03-08 12:33:27 -0500

    WIP Styled text.

--- app/routes/_marketing+/index.tsx
+++ app/routes/_marketing+/index.tsx
@@ -1,31 +1,24 @@
-import {
-	Tooltip,
-	TooltipContent,
-	TooltipProvider,
-	TooltipTrigger,
-} from '#app/components/ui/tooltip.tsx'
-import { cn } from '#app/utils/misc.tsx'
+// import { cn } from '#app/utils/misc.tsx'
 import { type Route } from './+types/index.ts'
-import { logos } from './logos/logos.ts'
 
 export const meta: Route.MetaFunction = () => [{ title: 'Purple Dreams' }]
 
 // Tailwind Grid cell classes lookup
-const columnClasses: Record<(typeof logos)[number]['column'], string> = {
-	1: 'xl:col-start-1',
-	2: 'xl:col-start-2',
-	3: 'xl:col-start-3',
-	4: 'xl:col-start-4',
-	5: 'xl:col-start-5',
-}
-const rowClasses: Record<(typeof logos)[number]['row'], string> = {
-	1: 'xl:row-start-1',
-	2: 'xl:row-start-2',
-	3: 'xl:row-start-3',
-	4: 'xl:row-start-4',
-	5: 'xl:row-start-5',
-	6: 'xl:row-start-6',
-}
+// const columnClasses: Record<(typeof logos)[number]['column'], string> = {
+// 	1: 'xl:col-start-1',
+// 	2: 'xl:col-start-2',
+// 	3: 'xl:col-start-3',
+// 	4: 'xl:col-start-4',
+// 	5: 'xl:col-start-5',
+// }
+// const rowClasses: Record<(typeof logos)[number]['row'], string> = {
+// 	1: 'xl:row-start-1',
+// 	2: 'xl:row-start-2',
+// 	3: 'xl:row-start-3',
+// 	4: 'xl:row-start-4',
+// 	5: 'xl:row-start-5',
+// 	6: 'xl:row-start-6',
+// }
 const love = `love`
 export default function Index() {
 	return (
