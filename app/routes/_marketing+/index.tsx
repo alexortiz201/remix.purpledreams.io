c7644d9d9b1d1d6305a87c08d9c0bcb4e878f282
Alexander Ortiz <alexanderortiz.201@gmail.com>
2025-03-08 12:55:14 -0500

    WIP Styled p.

--- app/routes/_marketing+/index.tsx
+++ app/routes/_marketing+/index.tsx
@@ -23,12 +23,12 @@ const love = `love`
 export default function Index() {
 	return (
 		<main className="font-poppins grid place-items-center px-4 py-16 xl:grid-cols-1 xl:gap-24 font-light">
-			<div className="flex max-w-md flex-col items-center text-center xl:order-2 xl:items-center">
+			<div className="flex max-w-md flex-col items-left xl:order-2 xl:items-left">
 				<h2 className='mt-8 animate-slide-top text-3xl text-foreground [animation-delay:0.3s] [animation-fill-mode:backwards] md:text-3xl xl:mt-4 xl:animate-slide-left xl:text-3xl xl:[animation-delay:0.8s] xl:[animation-fill-mode:backwards]'>
 					About
 				</h2>
 				<p data-paragraph className="mt-6 animate-slide-top text-xl/7 text-muted-foreground [animation-delay:0.8s] [animation-fill-mode:backwards] xl:mt-8 xl:animate-slide-left xl:text-xl/6 xl:leading-10 xl:[animation-delay:1s] xl:[animation-fill-mode:backwards]">
-					<span className='block text-center'>
+					<span className='block'>
 						Front end focus full stack engineer.
 					</span>
 					I <span className="color-pink">{ love }</span> building wonderful web experiences,
