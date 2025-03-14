e827d25f37431307ca0a0aaa8777f8aeef89ab6a
Alexander Ortiz <alexanderortiz.201@gmail.com>
2025-03-14 10:56:01 -0400

    Updated seed file. Prettified files.

--- app/components/canvas.tsx
+++ app/components/canvas.tsx
@@ -1,114 +1,121 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useRef } from 'react'
 
 type DotsType = {
-  nb: number
-  distance: number
-  d_radius: number
-  array: Dot[]
+	nb: number
+	distance: number
+	d_radius: number
+	array: Dot[]
 }
 
 class Dot {
-  x: number;
-  y: number;
-  vx: number;
-  vy: number;
-  radius: number;
-  canvas: HTMLCanvasElement;
-  ctx: CanvasRenderingContext2D;
-  dots: DotsType;
+	x: number
+	y: number
+	vx: number
+	vy: number
+	radius: number
+	canvas: HTMLCanvasElement
+	ctx: CanvasRenderingContext2D
+	dots: DotsType
 
-  constructor(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D, dots: DotsType){
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
-    this.vx = -.5 + Math.random();
-    this.vy = -.5 + Math.random();
-    this.radius = Math.random();
+	constructor(
+		canvas: HTMLCanvasElement,
+		ctx: CanvasRenderingContext2D,
+		dots: DotsType,
+	) {
+		this.x = Math.random() * canvas.width
+		this.y = Math.random() * canvas.height
+		this.vx = -0.5 + Math.random()
+		this.vy = -0.5 + Math.random()
+		this.radius = Math.random()
 
-    this.canvas = canvas
-    this.ctx = ctx
-    this.dots = dots
-  }
+		this.canvas = canvas
+		this.ctx = ctx
+		this.dots = dots
+	}
 
-  create() {
-    this.ctx.beginPath()
-    this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    this.ctx.fill()
-  }
+	create() {
+		this.ctx.beginPath()
+		this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+		this.ctx.fill()
+	}
 
-  animate() {
-    for (let i = 0; i < this.dots.nb; i++){
-      let dot = this.dots.array[i];
+	animate() {
+		for (let i = 0; i < this.dots.nb; i++) {
+			let dot = this.dots.array[i]
 
-      if (!dot) return
+			if (!dot) return
 
-      if (dot.y < 0||dot.y > this.canvas.height){
-        [dot.vx, dot.vy] = [dot.vx, -dot.vy]
-      } else if (dot.x < 0 || dot.x > this.canvas.width){
-        [dot.vx, dot.vy] = [-dot.vx, dot.vy]
-      }
-      dot.x += dot.vx
-      dot.y += dot.vy
-    }
-  }
+			if (dot.y < 0 || dot.y > this.canvas.height) {
+				;[dot.vx, dot.vy] = [dot.vx, -dot.vy]
+			} else if (dot.x < 0 || dot.x > this.canvas.width) {
+				;[dot.vx, dot.vy] = [-dot.vx, dot.vy]
+			}
+			dot.x += dot.vx
+			dot.y += dot.vy
+		}
+	}
 }
 
 // canvasInit(document.querySelector('canvas'))
 const canvasInit = (canvas: HTMLCanvasElement | null) => {
-  const ctxCanvas = canvas?.getContext('2d')
+	const ctxCanvas = canvas?.getContext('2d')
 
-  if (!canvas || !ctxCanvas) return
+	if (!canvas || !ctxCanvas) return
 
-  canvas.style.display = 'block';
+	canvas.style.display = 'block'
 
-  const color='#72cc96';
-  ctxCanvas.fillStyle = color
-  ctxCanvas.strokeStyle = color
-  ctxCanvas.lineWidth = .1
-  const mousePosition = {
-    x: 30*canvas.width/100,
-    y: 30*canvas.height/100,
-  }
-  const dots: DotsType = {
-    nb: 500,
-    distance: 80,
-    d_radius: 150,
-    array: [],
-  }
+	const color = '#72cc96'
+	ctxCanvas.fillStyle = color
+	ctxCanvas.strokeStyle = color
+	ctxCanvas.lineWidth = 0.1
+	const mousePosition = {
+		x: (30 * canvas.width) / 100,
+		y: (30 * canvas.height) / 100,
+	}
+	const dots: DotsType = {
+		nb: 500,
+		distance: 80,
+		d_radius: 150,
+		array: [],
+	}
 
-  const createDots = () => {
-    let dot = undefined
+	const createDots = () => {
+		let dot = undefined
 
-    ctxCanvas.clearRect(0, 0, canvas.width, canvas.height);
+		ctxCanvas.clearRect(0, 0, canvas.width, canvas.height)
 
-    for (let i = 0; i < dots.nb; i++){
-      dots.array.push(new Dot(canvas, ctxCanvas, dots));
-      dot = dots.array[i];
-      dot && dot.create();
-    }
+		for (let i = 0; i < dots.nb; i++) {
+			dots.array.push(new Dot(canvas, ctxCanvas, dots))
+			dot = dots.array[i]
+			dot && dot.create()
+		}
 
-    dot && dot.animate();
-  }
+		dot && dot.animate()
+	}
 
-  return setInterval(createDots, 1000 / 30);
+	return setInterval(createDots, 1000 / 30)
 }
 
 export const CanvasAnimation = ({ className }: { className?: string } = {}) => {
-  const canvasRef = useRef(null);
+	const canvasRef = useRef(null)
 
-  useEffect(() => {
-    const canvas = canvasRef.current
-    const animationTimerId = canvasInit(canvas)
+	useEffect(() => {
+		const canvas = canvasRef.current
+		const animationTimerId = canvasInit(canvas)
 
-    return () => {
-      clearInterval(animationTimerId)
-    }
-  }, [])
+		return () => {
+			clearInterval(animationTimerId)
+		}
+	}, [])
 
-  return (
-    <canvas className={className} ref={canvasRef} width="960" height="633"></canvas>
-  )
+	return (
+		<canvas
+			className={className}
+			ref={canvasRef}
+			width="960"
+			height="633"
+		></canvas>
+	)
 }
 
-export const dotInit = () => {
-
-}
+export const dotInit = () => {}
--- app/components/left-panel.tsx
+++ app/components/left-panel.tsx
@@ -1,57 +1,64 @@
-import { CanvasAnimation } from "./canvas"
-import { Icon } from "./ui/icon"
+import { CanvasAnimation } from './canvas'
+import { Icon } from './ui/icon'
 
 const PURPLEDREAMS_LINK = '/'
 const Content = () => {
-  return (
-    <div className="grid place-items-center px-4 py-16 xl:grid-cols-1 xl:gap-24 font-light">
-      <div className="flex max-w-md flex-col items-center text-center xl:order-2 xl:items-center">
-        <a
-          href={PURPLEDREAMS_LINK}
-          className="a__logo-link w-[60px]"
-        >
-          <img src="/img/logo04.png" />
-        </a>
-        <h1
-          data-heading
-          className="mt-2 text-4xl text-foreground md:text-5xl xl:mt-2 xl:text-6xl"
-        >
-          <a href={PURPLEDREAMS_LINK} className="border-animation">
-            <span className="border-animation font-extralight">purple</span>
-            <span className="border-animation font-medium">dreams</span>
-          </a>
-        </h1>
-        <div className="mt-6 text-xl/7 text-muted-foreground xl:mt-8 xl:text-xl/6 xl:leading-10 flex justify-between">
-          <ul className="list-none flex gap-2">
-            <li className="flex">
-              <a className="social-btn" href="https://linkedin.com/in/alexortiz201" target="_blank" rel="noopener noreferrer">
-                <Icon className="size-[1.5em]" name="linkedin-logo">
-                  <span className="sr-only">LinkedIn</span>
-                </Icon>
-              </a>
-            </li>
-            <li className="flex">
-              <a className="social-btn" href="https://github.com/alexortiz201" target="_blank" rel="noopener noreferrer">
-                <Icon className="size-[1.5em]" name="github-logo">
-                  <span className="sr-only">Github</span>
-                </Icon>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  )
+	return (
+		<div className="grid place-items-center px-4 py-16 font-light xl:grid-cols-1 xl:gap-24">
+			<div className="flex max-w-md flex-col items-center text-center xl:order-2 xl:items-center">
+				<a href={PURPLEDREAMS_LINK} className="a__logo-link w-[60px]">
+					<img src="/img/logo04.png" />
+				</a>
+				<h1
+					data-heading
+					className="mt-2 text-4xl text-foreground md:text-5xl xl:mt-2 xl:text-6xl"
+				>
+					<a href={PURPLEDREAMS_LINK} className="border-animation">
+						<span className="border-animation font-extralight">purple</span>
+						<span className="border-animation font-medium">dreams</span>
+					</a>
+				</h1>
+				<div className="mt-6 flex justify-between text-xl/7 text-muted-foreground xl:mt-8 xl:text-xl/6 xl:leading-10">
+					<ul className="flex list-none gap-2">
+						<li className="flex">
+							<a
+								className="social-btn"
+								href="https://linkedin.com/in/alexortiz201"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<Icon className="size-[1.5em]" name="linkedin-logo">
+									<span className="sr-only">LinkedIn</span>
+								</Icon>
+							</a>
+						</li>
+						<li className="flex">
+							<a
+								className="social-btn"
+								href="https://github.com/alexortiz201"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<Icon className="size-[1.5em]" name="github-logo">
+									<span className="sr-only">Github</span>
+								</Icon>
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	)
 }
 
-export const LeftPanel = ({ className } : { className?: string } = {}) => {
-  return (
-    <div className={`flex min-h-screen flex-col place-content-center relative ${className}`}>
-      <div className="flex flex-1 flex-col w-full absolute">
-        <Content />
-      </div>
-      <div className="min-h-screen left-panel h-full"></div>
-      <CanvasAnimation className="min-h-screen size-full absolute -z-10" />
-    </div>
-  )
+export const LeftPanel = ({ className }: { className?: string } = {}) => {
+	return (
+		<div className={`${className}`}>
+			<div className="absolute flex w-full flex-1 flex-col">
+				<Content />
+			</div>
+			<div className="left-panel h-full min-h-screen"></div>
+			<CanvasAnimation className="absolute -z-10 size-full min-h-screen" />
+		</div>
+	)
 }
