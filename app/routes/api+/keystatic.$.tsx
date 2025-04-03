aaeeb1c92c744d263824526831c21aa4763b4ce8
Alexander Ortiz <alexanderortiz.201@gmail.com>
2025-04-03 14:01:27 -0400

    Prisma gen

--- app/routes/api+/keystatic.$.tsx
+++ app/routes/api+/keystatic.$.tsx
@@ -2,5 +2,5 @@ import { handleLoader } from '@keystatic/remix/api'
 import { type ActionFunction, type LoaderFunction } from 'react-router'
 import config from '#/keystatic.config'
 
-export const loader: LoaderFunction = args => handleLoader({ config }, { context: args.context, ...args })
-export const action: ActionFunction = args => handleLoader({ config }, { context: args.context, ...args })
+export const loader: LoaderFunction = args => handleLoader({ config }, { ...args })
+export const action: ActionFunction = args => handleLoader({ config }, { ...args })
