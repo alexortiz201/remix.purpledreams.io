5193c8fd76f77dfd2ed620e0a2e1cdde974ed8fd
Alexander Ortiz <alexanderortiz.201@gmail.com>
2025-04-05 09:32:09 -0400

    Lock down keystatic.

--- app/routes/api+/keystatic.$.tsx
+++ app/routes/api+/keystatic.$.tsx
@@ -1,6 +1,13 @@
 import { handleLoader } from '@keystatic/remix/api'
 import { type ActionFunction, type LoaderFunction } from 'react-router'
 import config from '#/keystatic.config'
+import { requireUserWithRole } from '#app/utils/permissions.server.ts'
 
-export const loader: LoaderFunction = args => handleLoader({ config }, { ...args })
-export const action: ActionFunction = args => handleLoader({ config }, { ...args })
+export const loader: LoaderFunction = async (args) => {
+  await requireUserWithRole(args.request, 'admin')
+  return handleLoader({ config }, { ...args })
+}
+export const action: ActionFunction = async (args) => {
+  await requireUserWithRole(args.request, 'admin')
+  return handleLoader({ config }, { ...args })
+}
--- app/routes/keystatic+/$.tsx
+++ app/routes/keystatic+/$.tsx
@@ -1,5 +1,11 @@
-// app/routes/keystatic.$.tsx
 import { makePage } from '@keystatic/remix/ui'
 import config from '#/keystatic.config'
+import { requireUserWithRole } from '#app/utils/permissions.server.ts'
+import { type Route } from './+types/$'
+
+export async function loader({ request }: Route.LoaderArgs) {
+  await requireUserWithRole(request, 'admin')
+  return Response.json({ ok: true });
+}
 
 export default makePage(config)
